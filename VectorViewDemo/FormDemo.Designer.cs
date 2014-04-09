b3a483698d2f6da13f9988efefa941e4d8231ef7
RobinTang <sintrb@gmail.com>
2014-04-09 12:55:30 +0800

    Demo 1.1

--- VectorViewDemo/FormDemo.Designer.cs
+++ VectorViewDemo/FormDemo.Designer.cs
@@ -30,59 +30,21 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormDemo));
-            this.txtXmlFile = new System.Windows.Forms.TextBox();
-            this.btnReload = new System.Windows.Forms.Button();
-            this.btnSetXmlFile = new System.Windows.Forms.Button();
-            this.cbAutoReload = new System.Windows.Forms.CheckBox();
             this.tmRefresh = new System.Windows.Forms.Timer(this.components);
             this.vcMain = new Sin.VectorView.VectorControl();
+            this.msMain = new System.Windows.Forms.MenuStrip();
+            this.tsmiFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiFileOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiFileExportSVG = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsmiFileExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiControl = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiControlReload = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiControlAutoReload = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiHelp = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiHelpAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.msMain.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // txtXmlFile
-            // 
-            this.txtXmlFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtXmlFile.Location = new System.Drawing.Point(12, 12);
-            this.txtXmlFile.Name = "txtXmlFile";
-            this.txtXmlFile.Size = new System.Drawing.Size(452, 21);
-            this.txtXmlFile.TabIndex = 0;
-            // 
-            // btnReload
-            // 
-            this.btnReload.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnReload.Location = new System.Drawing.Point(546, 10);
-            this.btnReload.Name = "btnReload";
-            this.btnReload.Size = new System.Drawing.Size(75, 23);
-            this.btnReload.TabIndex = 1;
-            this.btnReload.Text = "重新加载";
-            this.btnReload.UseVisualStyleBackColor = true;
-            this.btnReload.Click += new System.EventHandler(this.btnReload_Click);
-            // 
-            // btnSetXmlFile
-            // 
-            this.btnSetXmlFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnSetXmlFile.Location = new System.Drawing.Point(470, 10);
-            this.btnSetXmlFile.Name = "btnSetXmlFile";
-            this.btnSetXmlFile.Size = new System.Drawing.Size(70, 23);
-            this.btnSetXmlFile.TabIndex = 2;
-            this.btnSetXmlFile.Text = "浏览...";
-            this.btnSetXmlFile.UseVisualStyleBackColor = true;
-            this.btnSetXmlFile.Click += new System.EventHandler(this.btnSetXmlFile_Click);
-            // 
-            // cbAutoReload
-            // 
-            this.cbAutoReload.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.cbAutoReload.AutoSize = true;
-            this.cbAutoReload.Checked = true;
-            this.cbAutoReload.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.cbAutoReload.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.cbAutoReload.Location = new System.Drawing.Point(627, 13);
-            this.cbAutoReload.Name = "cbAutoReload";
-            this.cbAutoReload.Size = new System.Drawing.Size(91, 20);
-            this.cbAutoReload.TabIndex = 4;
-            this.cbAutoReload.Text = "自动加载";
-            this.cbAutoReload.UseVisualStyleBackColor = true;
-            this.cbAutoReload.CheckedChanged += new System.EventHandler(this.cbAutoReload_CheckedChanged);
             // 
             // tmRefresh
             // 
@@ -95,25 +57,115 @@
                         | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
             this.vcMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.vcMain.Location = new System.Drawing.Point(12, 39);
+            this.vcMain.Location = new System.Drawing.Point(12, 27);
             this.vcMain.Name = "vcMain";
-            this.vcMain.Size = new System.Drawing.Size(706, 434);
+            this.vcMain.Size = new System.Drawing.Size(706, 446);
             this.vcMain.TabIndex = 3;
+            // 
+            // msMain
+            // 
+            this.msMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiFile,
+            this.tsmiControl,
+            this.tsmiHelp});
+            this.msMain.Location = new System.Drawing.Point(0, 0);
+            this.msMain.Name = "msMain";
+            this.msMain.Size = new System.Drawing.Size(730, 24);
+            this.msMain.TabIndex = 5;
+            this.msMain.Text = "menuStrip1";
+            // 
+            // tsmiFile
+            // 
+            this.tsmiFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiFileOpen,
+            this.tsmiFileExportSVG,
+            this.toolStripSeparator1,
+            this.tsmiFileExit});
+            this.tsmiFile.Name = "tsmiFile";
+            this.tsmiFile.Size = new System.Drawing.Size(43, 20);
+            this.tsmiFile.Text = "文件";
+            // 
+            // tsmiFileOpen
+            // 
+            this.tsmiFileOpen.Name = "tsmiFileOpen";
+            this.tsmiFileOpen.Size = new System.Drawing.Size(152, 22);
+            this.tsmiFileOpen.Text = "打开...";
+            this.tsmiFileOpen.Click += new System.EventHandler(this.tsmiFileOpen_Click);
+            // 
+            // tsmiFileExportSVG
+            // 
+            this.tsmiFileExportSVG.Name = "tsmiFileExportSVG";
+            this.tsmiFileExportSVG.Size = new System.Drawing.Size(152, 22);
+            this.tsmiFileExportSVG.Text = "导出SVG...";
+            this.tsmiFileExportSVG.Visible = false;
+            this.tsmiFileExportSVG.Click += new System.EventHandler(this.tsmiFileExportSVG_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // tsmiFileExit
+            // 
+            this.tsmiFileExit.Name = "tsmiFileExit";
+            this.tsmiFileExit.Size = new System.Drawing.Size(152, 22);
+            this.tsmiFileExit.Text = "退出";
+            this.tsmiFileExit.Click += new System.EventHandler(this.tsmiFileExit_Click);
+            // 
+            // tsmiControl
+            // 
+            this.tsmiControl.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiControlReload,
+            this.tsmiControlAutoReload});
+            this.tsmiControl.Name = "tsmiControl";
+            this.tsmiControl.Size = new System.Drawing.Size(43, 20);
+            this.tsmiControl.Text = "控制";
+            // 
+            // tsmiControlReload
+            // 
+            this.tsmiControlReload.Name = "tsmiControlReload";
+            this.tsmiControlReload.Size = new System.Drawing.Size(122, 22);
+            this.tsmiControlReload.Text = "重新加载";
+            this.tsmiControlReload.Click += new System.EventHandler(this.tsmiControlReload_Click);
+            // 
+            // tsmiControlAutoReload
+            // 
+            this.tsmiControlAutoReload.Checked = true;
+            this.tsmiControlAutoReload.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.tsmiControlAutoReload.Name = "tsmiControlAutoReload";
+            this.tsmiControlAutoReload.Size = new System.Drawing.Size(122, 22);
+            this.tsmiControlAutoReload.Text = "自动加载";
+            this.tsmiControlAutoReload.Click += new System.EventHandler(this.tsmiControlAutoReload_Click);
+            // 
+            // tsmiHelp
+            // 
+            this.tsmiHelp.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiHelpAbout});
+            this.tsmiHelp.Name = "tsmiHelp";
+            this.tsmiHelp.Size = new System.Drawing.Size(43, 20);
+            this.tsmiHelp.Text = "帮助";
+            // 
+            // tsmiHelpAbout
+            // 
+            this.tsmiHelpAbout.Name = "tsmiHelpAbout";
+            this.tsmiHelpAbout.Size = new System.Drawing.Size(98, 22);
+            this.tsmiHelpAbout.Text = "关于";
+            this.tsmiHelpAbout.Click += new System.EventHandler(this.tsmiHelpAbout_Click);
             // 
             // FormDemo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(730, 485);
-            this.Controls.Add(this.cbAutoReload);
             this.Controls.Add(this.vcMain);
-            this.Controls.Add(this.btnSetXmlFile);
-            this.Controls.Add(this.btnReload);
-            this.Controls.Add(this.txtXmlFile);
+            this.Controls.Add(this.msMain);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.msMain;
             this.Name = "FormDemo";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "VectorViewDemo";
+            this.msMain.ResumeLayout(false);
+            this.msMain.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -121,12 +173,19 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox txtXmlFile;
-        private System.Windows.Forms.Button btnReload;
-        private System.Windows.Forms.Button btnSetXmlFile;
         private Sin.VectorView.VectorControl vcMain;
-        private System.Windows.Forms.CheckBox cbAutoReload;
         private System.Windows.Forms.Timer tmRefresh;
+        private System.Windows.Forms.MenuStrip msMain;
+        private System.Windows.Forms.ToolStripMenuItem tsmiFile;
+        private System.Windows.Forms.ToolStripMenuItem tsmiFileOpen;
+        private System.Windows.Forms.ToolStripMenuItem tsmiFileExportSVG;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem tsmiFileExit;
+        private System.Windows.Forms.ToolStripMenuItem tsmiControl;
+        private System.Windows.Forms.ToolStripMenuItem tsmiControlReload;
+        private System.Windows.Forms.ToolStripMenuItem tsmiControlAutoReload;
+        private System.Windows.Forms.ToolStripMenuItem tsmiHelp;
+        private System.Windows.Forms.ToolStripMenuItem tsmiHelpAbout;
     }
 }
 
--- VectorViewDemo/FormDemo.cs
+++ VectorViewDemo/FormDemo.cs
@@ -12,44 +12,72 @@ namespace VectorViewDemo
 {
     public partial class FormDemo : Form
     {
-        public String VERSION = "1.0";
+        public String VERSION = "1.1";
+
         public FormDemo()
         {
             InitializeComponent();
 
             this.Text = this.Text + " " + VERSION;
 
-            this.txtXmlFile.Text = Environment.CurrentDirectory + "\\" + "data.xml";
-            ofd.FileName = this.txtXmlFile.Text;
-
-
-            btnReload_Click(null, null);
-            this.tmRefresh.Enabled = this.cbAutoReload.Checked;
+            this.tmRefresh.Enabled = this.tsmiControlAutoReload.Checked;
         }
 
         private OpenFileDialog ofd = new OpenFileDialog();
-        private void btnSetXmlFile_Click(object sender, EventArgs e)
+
+        private DateTime PreWriteTime;
+        private void tmRefresh_Tick(object sender, EventArgs e)
         {
-            if (ofd.ShowDialog() == DialogResult.OK)
+            if (tsmiControlAutoReload.Checked == false)
             {
-                this.txtXmlFile.Text = ofd.FileName;
-                btnReload_Click(sender, e);
+                tmRefresh.Enabled = false;
+                return;
+            }
+            if (ofd.FileName == null || ofd.FileName.Length == 0)
+                return;
+            lock (ofd)
+            {
+                FileInfo fi = new FileInfo(ofd.FileName);
+                if (PreWriteTime == null || PreWriteTime.CompareTo(fi.LastWriteTime) != 0)
+                {
+                    PreWriteTime = fi.LastWriteTime;
+                    tsmiControlReload_Click(null, null);
+                }
             }
         }
 
-        private void btnReload_Click(object sender, EventArgs e)
+        private void tsmiFileOpen_Click(object sender, EventArgs e)
         {
-            if (this.txtXmlFile.Text.Trim().Length == 0)
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                tsmiControlReload_Click(null, null);
+            }
+        }
+
+        private void tsmiFileExportSVG_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tsmiFileExit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tsmiControlReload_Click(object sender, EventArgs e)
+        {
+            if (ofd.FileName==null || ofd.FileName.Length == 0)
                 return;
+            this.Text = ofd.FileName;
 #if DEBUG
-            List<VectorObject> vos = ParseUtils.ParseXMLFile(this.txtXmlFile.Text);
+            List<VectorObject> vos = ParseUtils.ParseXMLFile(ofd.FileName);
             this.vcMain.VectorObjects.Clear();
             this.vcMain.VectorObjects.AddRange(vos);
             this.vcMain.Invalidate();
 #else
             try
             {
-                List<VectorObject> vos = ParseUtils.ParseXMLFile(this.txtXmlFile.Text);
+                List<VectorObject> vos = ParseUtils.ParseXMLFile(ofd.FileName);
                 this.vcMain.VectorObjects.Clear();
                 this.vcMain.VectorObjects.AddRange(vos);
                 this.vcMain.Invalidate();
@@ -61,29 +89,15 @@ namespace VectorViewDemo
 #endif
         }
 
-
-        private DateTime PreWriteTime;
-        private void tmRefresh_Tick(object sender, EventArgs e)
+        private void tsmiControlAutoReload_Click(object sender, EventArgs e)
         {
-            if (cbAutoReload.Checked == false)
-            {
-                tmRefresh.Enabled = false;
-                return;
-            }
-            lock (this.txtXmlFile)
-            {
-                FileInfo fi = new FileInfo(this.txtXmlFile.Text);
-                if (PreWriteTime == null || PreWriteTime.CompareTo(fi.LastWriteTime) != 0)
-                {
-                    PreWriteTime = fi.LastWriteTime;
-                    btnReload_Click(null, null);
-                }
-            }
+            tsmiControlAutoReload.Checked = !tsmiControlAutoReload.Checked;
+            this.tmRefresh.Enabled = this.tsmiControlAutoReload.Checked;
         }
 
-        private void cbAutoReload_CheckedChanged(object sender, EventArgs e)
+        private void tsmiHelpAbout_Click(object sender, EventArgs e)
         {
-            this.tmRefresh.Enabled = this.cbAutoReload.Checked;
+            new AboutBox().ShowDialog();
         }
     }
 }
